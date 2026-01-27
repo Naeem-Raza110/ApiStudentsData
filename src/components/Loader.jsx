ce79829d04e587f5936bdbd706f9daa72f9abd5d
Naeem-Raza110 <nraza7225@gmail.com>
2026-01-27 15:35:01 +0500

    add some changes to loader component

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,8 +1,8 @@
 const Loader = () => {
   return (
-    <div className="flex h-screen flex-col items-center justify-center">
-      <div className="h-12 w-12 animate-spin rounded-full border-4 border-slate-700 "></div>
-      <p className="mt-4">Loading students...</p>
+    <div className="flex h-screen flex-col items-center justify-center ">
+      <div className="h-12 w-12 animate-spin rounded-full border-4 border-slate-700 border-t-black"></div>
+      <p className="mt-4 ">Loading students...</p>
     </div>
   );
 };
